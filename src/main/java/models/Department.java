eeb293ac035568647e221a0953d4247e423277c2
atembamanu <atembaemm07@gmail.com>
2019-10-28 07:59:17 +0300

    Create a POJO class with constructors, getters and setters to allow the test to pass.

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,54 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    private String departmentName;
+    private  String description;
+    private int empNo;
+
+    public Department(String departmentName, String description, int empNo) {
+        this.departmentName = departmentName;
+        this.description = description;
+        this.empNo = empNo;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(int empNo) {
+        this.empNo = empNo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return getEmpNo() == that.getEmpNo() &&
+                getDepartmentName().equals(that.getDepartmentName()) &&
+                getDescription().equals(that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDepartmentName(), getDescription(), getEmpNo());
+    }
 }
