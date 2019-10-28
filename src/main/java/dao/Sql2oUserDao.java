5ffec86cdeca548dcd61a4c4d8ef9b5c796ef0ea
atembamanu <atembaemm07@gmail.com>
2019-10-28 18:42:58 +0300

    Implement the code to make the tests pass

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -1,7 +1,9 @@
 package dao;
 
 import models.User;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -11,16 +13,54 @@ public class Sql2oUserDao implements UserDao{
 
     @Override
     public void addUser(User user) {
-
+        String sql = "INSERT INTO users (firstName, secondName, designation, role, department_id) VALUES (:firstName, :secondName, :designation, :role, :department_id)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM users")
+                    .executeAndFetch(User.class);
+        }
     }
 
     @Override
     public List<User> getUserById(int id) {
-        return null;
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM users WHERE id = :userId")
+                    .addParameter("userId", id)
+                    .executeAndFetch(User.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from users WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteAllUsers() {
+        String sql = "DELETE from users";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 }
