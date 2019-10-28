5759e6c9c0a507ba5d01b91cebc4beccaa987a89
atembamanu <atembaemm07@gmail.com>
2019-10-28 05:51:07 +0300

    Create the tests for the user POJO: if it instantiates correctly, and getters are cool.

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,7 +1,55 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UserTest {
+
+    @Test
+    public  void  getFirstName_returnsCorrectFirstName(){
+        User user = setupUser();
+        assertEquals("Emmanuel", user.getFirstName());
+
+    }
+
+    @Test
+    public void getSecondName_returnsCorrectSecondName() {
+        User user = setupUser();
+        assertEquals("Atemba", user.getSecondName());
+
+    }
+
+    @Test
+    public  void  getEmail_returnsCorrectEmail(){
+        User user = setupUser();
+        assertEquals("atembaemm07@gmail.com", user.getEmail());
+
+    }
+
+    @Test
+    public void getPosition_returnsCorrectPosition() {
+        User user = setupUser();
+        assertEquals("CEO", user.getPosition());
+
+    }
+    @Test
+    public  void  getRole_returnsCorrectRole(){
+        User user = setupUser();
+        assertEquals("Making major corporate decisions", user.getRole());
+
+    }
+
+    @Test
+    public void getDepartmentId_returnsCorrectDepartment() {
+        User user = setupUser();
+        assertEquals(1, user.getDepartment_id());
+
+    }
+
+
+    public User setupUser(){
+        return  new User("Emmanuel", "Atemba", "atembaemm07@gmail.com", "CEO", "Making major corporate decisions", 1);
+    }
 
 }
