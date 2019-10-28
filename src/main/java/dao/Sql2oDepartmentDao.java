6467ef0403d16a824c31d89594a194d6f3bcbf3d
atembamanu <atembaemm07@gmail.com>
2019-10-28 18:45:39 +0300

    implement all methods of the Dao interface

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -1,8 +1,67 @@
 package dao;
 
+import models.Department;
+import models.User;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
-public class Sql2oDepartmentDao {
+import java.util.List;
+
+public class Sql2oDepartmentDao implements   DepartmentDao{
     private final Sql2o sql2o;
     public Sql2oDepartmentDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public void addDepartment(Department user) {
+        String sql = "INSERT INTO departments (name, description, empNo) VALUES (:name, :description, :empNo)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM departments")
+                    .executeAndFetch(Department.class);
+        }
+    }
+
+    @Override
+    public List<Department> getDepartmentById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM departments WHERE id = :deptId")
+                    .addParameter("deptId", id)
+                    .executeAndFetch(Department.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from departments WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteAllDepartments() {
+        String sql = "DELETE from departments";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
 }
