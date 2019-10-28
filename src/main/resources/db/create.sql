807b4c1c588ed5cb5114933cfa801263f1fdd654
atembamanu <atembaemm07@gmail.com>
2019-10-28 14:33:31 +0300

    create sql file for creating database tables

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,24 @@
+    SET MODE PostgreSQL;
+
+    CREATE TABLE IF NOT EXISTS users (
+    id int serial PRIMARY KEY,
+    firstName VARCHAR,
+    secondName VARCHAR,
+    designation VARCHAR,
+    role VARCHAR,
+    deptId int
+    );
+
+    CREATE TABLE IF NOT EXISTS departments (
+    id int serial PRIMARY KEY,
+    name VARCHAR,
+    description VARCHAR,
+    empNo int
+    );
+
+    CREATE TABLE IF NOT EXISTS news (
+    id int serial PRIMARY KEY,
+    title VARCHAR,
+    content VARCHAR,
+    category VARCHAR
+    );
