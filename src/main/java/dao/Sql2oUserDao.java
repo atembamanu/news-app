396cc2fa2f046f6b61f76dcf59f78f07e77e8fb7
atembamanu <atembaemm07@gmail.com>
2019-10-28 15:40:20 +0300

    Implement all UserDao interface methods

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -1,9 +1,26 @@
 package dao;
 
+import models.User;
 import org.sql2o.Sql2o;
 
-public class Sql2oUserDao {
+import java.util.List;
+
+public class Sql2oUserDao implements UserDao{
     private final Sql2o sql2o;
     public Sql2oUserDao(Sql2o sql2o) { this.sql2o = sql2o; }
 
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public List<User> getUserById(int id) {
+        return null;
+    }
 }
