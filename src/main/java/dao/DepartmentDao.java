652e349d25225ad21f7a20fd7a1d052faed1348b
atembamanu <atembaemm07@gmail.com>
2019-10-28 18:45:06 +0300

    create an interface to provide required methods

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,15 @@
 package dao;
 
+import models.Department;
+import models.User;
+
+import java.util.List;
+
 public interface DepartmentDao {
+
+    void addDepartment(Department user);
+    List<Department> getAllDepartments();
+    List<Department> getDepartmentById(int id);
+    void deleteById(int id);
+    void deleteAllDepartments();
 }
