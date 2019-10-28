6a631b8df25f9ba36d46b105dbf57341bc0e911c
atembamanu <atembaemm07@gmail.com>
2019-10-28 07:58:24 +0300

    Create the tests for the Department POJO: if it instantiates correctly, and if getters are working correctly.

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,7 +1,32 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DepartmentTest {
+
+    @Test
+    public  void getDepartmentName_returnsCorrectDepartmentName(){
+        Department department = setupDepartment();
+        assertEquals("Managerial", department.getDepartmentName());
+
+    }
+    @Test
+    public  void getDescription_returnsCorrectDescription(){
+        Department department = setupDepartment();
+        assertEquals("Organisations' decision making body", department.getDescription());
+
+    }
+    @Test
+    public  void getEmpNo_returnsCorrectEmpNo(){
+        Department department = setupDepartment();
+        assertEquals(23, department.getEmpNo());
+
+    }
+
+    public  Department setupDepartment(){
+        return  new Department("Managerial", "Organisations' decision making body", 23);
+    }
 
 }
