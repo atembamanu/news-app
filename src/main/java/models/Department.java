56d3257692ac877c8ce0e3b81d69a6681e8f3ffe
atembamanu <atembaemm07@gmail.com>
2019-10-28 18:44:25 +0300

    add id attribute to Department POJO class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -6,6 +6,7 @@ public class Department {
     private String departmentName;
     private  String description;
     private int empNo;
+    private  int id;
 
     public Department(String departmentName, String description, int empNo) {
         this.departmentName = departmentName;
@@ -37,18 +38,27 @@ public class Department {
         this.empNo = empNo;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Department)) return false;
         Department that = (Department) o;
         return getEmpNo() == that.getEmpNo() &&
+                getId() == that.getId() &&
                 getDepartmentName().equals(that.getDepartmentName()) &&
                 getDescription().equals(that.getDescription());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getDepartmentName(), getDescription(), getEmpNo());
+        return Objects.hash(getDepartmentName(), getDescription(), getEmpNo(), getId());
     }
 }
